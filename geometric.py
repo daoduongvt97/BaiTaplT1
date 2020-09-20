b4f0dd0eb863e16ca461d79789f763b918df5f72
daoduongvt97 <34453125+daoduongvt97@users.noreply.github.com>
2020-09-20 22:08:15 +0700

    Update

--- geometric.py
+++ geometric.py
@@ -4,6 +4,11 @@ def prop(n, p):
 def infoMeasure(n,p):
     return -math.log(prop(n,p))
 def sumProb(N,p):
+    """
+        sumProb(10,0.5) = 0.9990234375,
+        sumProb(100,0.5) = sumProb(100,0.5) = 1.0
+        => tổng xác suất của phân bố geometric tiến dần về 1
+    """
     sum = 0.0
     for i in range(1,N+1):
         sum += prop(i, p)
